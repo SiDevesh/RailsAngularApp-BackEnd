e3b820e65fcf790c153386f62f6b26be10d2137f
SiDevesh <sid.swapnildevesh@gmail.com>
2016-07-25 09:18:05 +0530

    Added devisetokenauth and simultaneously user model

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :api, defaults: {format: :json} do
+    scope :v1 do
+      mount_devise_token_auth_for 'User', at: 'auth'
+      resources :users, :only => [:show, :index]
+    end
+  end
+
 end
