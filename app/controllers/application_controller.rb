381e0af597c3a7cffb0280fcfb89cce4c7a6672a
SiDevesh <sid.swapnildevesh@gmail.com>
2016-09-15 03:15:01 +0530

    Allow full conventional authentication methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,10 @@
 class ApplicationController < ActionController::API
-  protect_from_forgery with: :null_session
+  #protect_from_forgery with: :null_session
   include DeviseTokenAuth::Concerns::SetUserByToken
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    #devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:name, :email, :phoneno, :whoyou, :whatteach, :whatlearn, :address, :country, :state, :pincode, :city, :password, :password_confirmation, :current_password, :profic ) }
+  end
+
 end
--- config/application.rb
+++ config/application.rb
@@ -26,5 +26,19 @@ module NewlApi
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    config.middleware.insert_before 0, Rack::Cors do
+		allow do
+        	origins '*'
+        	resource '*',
+          	:headers => :any,
+          	:expose  => ['access-token', 'expiry', 'token-type', 'uid', 'client'],
+          	:methods => [:get, :post, :options, :delete, :put]
+        end
+    end
+
+    #config.middleware.use ActionDispatch::Cookies
+    #config.middleware.use ActionDispatch::Session::CookieStore
+
   end
 end
